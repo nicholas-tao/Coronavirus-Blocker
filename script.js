4fae2c2d53624426a6eaa738bfe5eedfa93e9ef9
nicholas-tao <jacksana1@gmail.com>
2020-03-21 14:58:08 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+replaceText (document.body)
+
+function replaceText (element) {
+    if (element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText) 
+    } else if (element.nodeType === Text.TEXT_NODE){
+        element.textContent = element.textContent.replace(/coronavirus/gi,'████')
+        element.textContent = element.textContent.replace(/covid-19/gi,'████')
+    }
+}
